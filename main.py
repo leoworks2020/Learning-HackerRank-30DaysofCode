f82dda2c1bf4208e7a373f1dd06509b9e2cd4fd4
leoworks2020 <leoworks2020@gmail.com>
2023-07-18 10:14:30 -0300

    Solved Day 05 Challenge

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 # Hacker Rank 30 Days of Code Challenges.
 
 # Here in this project each Day Challenge will be stored in a separate .py file
+
+# Train for classes
+
+class Mycar:
+    def __init__(self, vendor, model):
+        self.vendor = vendor
+        self.model = model
+
+
+car1 = Mycar("Honda", "Fit")
+print(car1.vendor)
+print(car1.model)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)

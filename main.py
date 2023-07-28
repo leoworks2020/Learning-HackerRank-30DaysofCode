3a1b03a5857b02cce154cf77bd53798883ec6675
leoworks2020 <leoworks2020@gmail.com>
2023-07-27 23:40:11 -0300

    Solved Day 14 Challenge

--- main.py
+++ main.py
@@ -33,16 +33,21 @@ for w in range(len(string)):
     print(string[w])
 
 # Dictionary
+print('Dictionary: Input a integer:')
 n=int(input())
 dict={}
 for i in range(n):
+    print("Please inform dictionary key and value separated by space")
     key,value=input().split()
     dict.update({key:value})
     while True :
         try:
+            print("Plese inform key to be found in Dict")
             s=input()
             if(s in dict):
                 print(s+"="+dict[s])
+            elif s == "exit":
+                break
             else:
                 print("Not found")
         except:
@@ -53,3 +58,21 @@ list_1 = [1,2,3]
 list_2 = [4,5,6]
 list_3 = zip(list_1, list_2)
 print(list_3)
+
+# Class and Reverse list
+print('Class and Reverse List...')
+class MyClass:
+    def __init__(self, data: tuple or list):
+        self.data = data
+
+    def my_method(self):
+        if isinstance(self.data, tuple):
+            return self.data[1:][::-1]
+        else:
+            return self.data[::-3]
+
+words = ['apple', 'banana', 'cherry']
+obj = MyClass(words)
+
+print(obj.my_method())
+

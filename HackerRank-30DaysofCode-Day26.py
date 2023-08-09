c4b560d96c9df7ec717e0fee19f821ffd90deff2
leoworks2020 <leoworks2020@gmail.com>
2023-08-09 19:48:38 -0300

    Solved Day 26 Challenge

--- HackerRank-30DaysofCode-Day26.py
+++ HackerRank-30DaysofCode-Day26.py
@@ -71,3 +71,50 @@ Challenge Start Date =  2023-08-08
 Challenge End Date   =  2023-08-
 """
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+def calc_fine(return_date_list, due_date_list):
+    calculated_fine = 0
+    fine_count = 0
+    return_date_day = int(return_date_list[0])
+    return_date_month = int(return_date_list[1])
+    return_date_year = int(return_date_list[2])
+    return_date_serial = convert_date_to_serial(return_date_day, return_date_month, return_date_year)
+
+    due_date_day = int(due_date_list[0])
+    due_date_month = int(due_date_list[1])
+    due_date_year = int(due_date_list[2])
+    due_date_serial = convert_date_to_serial(due_date_day, due_date_month, due_date_year)
+
+    #Condition 1 - No Fine (Book is returned before due date)
+    if return_date_serial <= due_date_serial:
+        return calculated_fine
+
+    if return_date_year == due_date_year:
+        if return_date_month == due_date_month:
+            if return_date_day > due_date_day: # Condition 2 - 15 Hackos Fine - Day(s) late within same month and year
+                fine_count = return_date_day - due_date_day
+                return 15 * fine_count
+        else: # Condition 3 - 500 Hackos Fine - Month(s) late within same year
+            fine_count = return_date_month - due_date_month
+            return 500 * fine_count
+    else: # Condition 4 - 10000 Hackos Fixed Fine - Late over the calendar year
+        return 10000
+
+def convert_date_to_serial(day, month, year):
+    # Specifying offset value i.e.,
+    # the date value for the date of 1900-01-00
+    offset = 693594
+
+    input_date = date(year, month, day)
+    n = input_date.toordinal()
+    return (n - offset)
+
+
+
+from datetime import date
+
+DBG = 0
+
+returned_date_list = input().split(" ")
+due_date_list = input().split(" ")
+fine = calc_fine(returned_date_list, due_date_list)
+print(fine)
